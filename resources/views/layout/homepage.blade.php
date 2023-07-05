4e7f8ac4563f4243f36d1f1b51347a6a9b885c39
tranthuy240814 <tranthuy240814@gmail.com>
2023-07-05 10:53:50 +0700

    master layout

---
+++ resources/views/layout/homepage.blade.php
@@ -0,0 +1,96 @@
+@extends('welcome')
+@section('content')
+<div style="padding-bottom:20px;position:relative">
+    <div class="css-1iuj5ih">
+        <div class="css-cydm8w">
+            <div class="titleContainer css-x933z2">
+                <h2 class="carouselTitle">Recommended for you</h2>
+            </div>
+            <div class="prime-carousel css-1nehx92">
+                <ul class="prime-carousel-container css-lpurl3">
+                    <li class="primeCarouselLi"><a class="css-h13zhk" href="https://www.crazygames.com/game/bloxdhop-io">
+                            <div class="gameThumbTitleContainer">Bloxd.io</div><img class="GameThumbImage" loading="eager" src="https://images.crazygames.com/games/bloxdhop-io/cover-1671463581901.png?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;fit=crop" decoding="auto" alt="Bloxd.io" srcset="https://images.crazygames.com/games/bloxdhop-io/cover-1671463581901.png?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=428&amp;h=237&amp;fit=crop 429w,
+https://images.crazygames.com/games/bloxdhop-io/cover-1671463581901.png?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=436&amp;h=241&amp;fit=crop 437w,
+https://images.crazygames.com/games/bloxdhop-io/cover-1671463581901.png?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=448&amp;h=248&amp;fit=crop 449w,
+https://images.crazygames.com/games/bloxdhop-io/cover-1671463581901.png?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=471&amp;h=229&amp;fit=crop 472w,
+https://images.crazygames.com/games/bloxdhop-io/cover-1671463581901.png?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=519&amp;h=289&amp;fit=crop 520w,
+https://images.crazygames.com/games/bloxdhop-io/cover-1671463581901.png?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=675&amp;h=378&amp;fit=crop 676w,
+https://images.crazygames.com/games/bloxdhop-io/cover-1671463581901.png?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=710&amp;h=398&amp;fit=crop 711w" sizes="(min-width:3000px) 710px,
+(min-width:2100px) 675px,
+(min-width:1200px) 519px,
+(min-width:1000px) 428px,
+(min-width:800px) 436px,
+(min-width:600px) 448px,
+471px" fetchpriority="high">
+                        </a></li>
+                </ul><button class="arrow css-1pzb6vf" aria-label="Right arrow"></button>
+            </div>
+        </div>
+        <div class="css-zyoi9w">
+            <div class="titleContainer css-18689kv">
+                <h2 class="carouselTitle">Featured games</h2>
+            </div>
+            <div class="prime-carousel css-1nehx92">
+                <ul class="prime-carousel-container css-lpurl3">
+                    <li class="primeCarouselLi"><a class="css-h13zhk" href="https://www.crazygames.com/game/stickman-vs-villager-save-the-girl">
+                            <div class="gameThumbTitleContainer">Stickman vs Villager: Save the Girl</div><img class="GameThumbImage" loading="eager" src="https://images.crazygames.com/stickman-vs-villager-save-the-girl/20230620083148/stickman-vs-villager-save-the-girl-cover?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;fit=crop" decoding="auto" alt="Stickman vs Villager: Save the Girl" srcset="https://images.crazygames.com/stickman-vs-villager-save-the-girl/20230620083148/stickman-vs-villager-save-the-girl-cover?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=206&amp;h=116&amp;fit=crop 207w,
+https://images.crazygames.com/stickman-vs-villager-save-the-girl/20230620083148/stickman-vs-villager-save-the-girl-cover?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=209&amp;h=118&amp;fit=crop 210w,
+https://images.crazygames.com/stickman-vs-villager-save-the-girl/20230620083148/stickman-vs-villager-save-the-girl-cover?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=215&amp;h=121&amp;fit=crop 216w,
+https://images.crazygames.com/stickman-vs-villager-save-the-girl/20230620083148/stickman-vs-villager-save-the-girl-cover?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=224&amp;h=126&amp;fit=crop 225w,
+https://images.crazygames.com/stickman-vs-villager-save-the-girl/20230620083148/stickman-vs-villager-save-the-girl-cover?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=252&amp;h=142&amp;fit=crop 253w,
+https://images.crazygames.com/stickman-vs-villager-save-the-girl/20230620083148/stickman-vs-villager-save-the-girl-cover?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=329&amp;h=185&amp;fit=crop 330w,
+https://images.crazygames.com/stickman-vs-villager-save-the-girl/20230620083148/stickman-vs-villager-save-the-girl-cover?auto=format%2Ccompress&amp;q=45&amp;cs=strip&amp;ch=DPR&amp;w=336&amp;h=189&amp;fit=crop 337w" sizes="(min-width:3000px) 336px,
+(min-width:2100px) 329px,
+(min-width:1200px) 252px,
+(min-width:1000px) 206px,
+(min-width:800px) 209px,
+(min-width:600px) 215px,
+224px">
+                        </a></li>
+                    <li class="primeCarouselLi"><span class="MuiSkeleton-root MuiSkeleton-rectangular skeleton css-d7g3uh" style="width:calc(100% - 2px);height:calc(100% - 2px)"></span></li>
+                    <li class="primeCarouselLi"><span class="MuiSkeleton-root MuiSkeleton-rectangular skeleton css-d7g3uh" style="width:calc(100% - 2px);height:calc(100% - 2px)"></span></li>
+                    <li class="primeCarouselLi"><span class="MuiSkeleton-root MuiSkeleton-rectangular skeleton css-d7g3uh" style="width:calc(100% - 2px);height:calc(100% - 2px)"></span></li>
+                    <li class="primeCarouselLi"><span class="MuiSkeleton-root MuiSkeleton-rectangular skeleton css-d7g3uh" style="width:calc(100% - 2px);height:calc(100% - 2px)"></span></li>
+                    <li class="primeCarouselLi"><span class="MuiSkeleton-root MuiSkeleton-rectangular skeleton css-d7g3uh" style="width:calc(100% - 2px);height:calc(100% - 2px)"></span></li>
+                    <li class="primeCarouselLi"><span class="MuiSkeleton-root MuiSkeleton-rectangular skeleton css-d7g3uh" style="width:calc(100% - 2px);height:calc(100% - 2px)"></span></li>
+                    <li class="primeCarouselLi"><span class="MuiSkeleton-root MuiSkeleton-rectangular skeleton css-d7g3uh" style="width:calc(100% - 2px);height:calc(100% - 2px)"></span></li>
+                    <li class="primeCarouselLi"><span class="MuiSkeleton-root MuiSkeleton-rectangular skeleton css-d7g3uh" style="width:calc(100% - 2px);height:calc(100% - 2px)"></span></li>
+                </ul><button class="arrow css-1pzb6vf" aria-label="Right arrow"></button>
+            </div>
+        </div>
+        <div style="overflow-anchor:auto" class="css-41foc0">
+            <div style="overflow:hidden" class="css-1xcs8fm">
+                <div>
+                    <h2>Play the Newest Games Instantly</h2>
+                    <p>CrazyGames features the latest and best free online games. You can enjoy playing fun games without interruptions from downloads, intrusive ads, or pop-ups. Just load up your favorite games instantly in your web browser and enjoy the experience.</p>
+                    <p>You can play our games on desktop mobile devices. That includes everything from desktop PCs, laptops, and Chromebooks, to the latest smartphones and tablets from Apple and Android. </p>
+                    <h2>Online Games at CrazyGames</h2>
+                    <p>There are plenty of online multiplayer games with active communities on CrazyGames. You can find many of the best free multiplayer titles on our<a target="_blank" href="https://www.crazygames.com/c/io"> .io games </a>page. In these games, you can play with your friends online and with other people from around the world, no matter where you are.</p>
+                    <h2>Play our Best Games</h2>
+                    <p>CrazyGames has over 7000 fun games in every genre you can imagine. Some of our most popular games are:</p>
+                    <ul>
+                        <li><a target="_blank" href="https://www.crazygames.com/game/shellshockersio">Shell Shockers</a></li>
+                        <li><a target="_blank" href="https://www.crazygames.com/game/bullet-force-multiplayer">Bullet Force</a></li>
+                        <li><a target="_blank" href="https://www.crazygames.com/game/uno-online">Uno Online</a></li>
+                        <li><a target="_blank" href="https://www.crazygames.com/game/drift-hunters">Drift Hunters</a></li>
+                        <li><a target="_blank" href="https://www.crazygames.com/game/moto-x3m">Moto X3M</a></li>
+                        <li><a target="_blank" href="https://www.crazygames.com/game/smash-karts">Smash Karts</a></li>
+                    </ul>
+                    <h2>Explore by Genre</h2>
+                    <p>You’ll find the main categories like <a target="_blank" href="https://www.crazygames.com/c/clicker">clicker</a>, <a target="_blank" href="https://www.crazygames.com/c/driving">driving games</a>, and <a target="_blank" href="https://www.crazygames.com/c/shooting">shooting games</a> at the top of any page, but there’s also a range of subcategories that will help you find the perfect game. Popular tags include <a target="_blank" href="https://www.crazygames.com/t/car">car games</a>, <a target="_blank" href="https://www.crazygames.com/t/minecraft">Minecraft</a>, <a target="_blank" href="https://www.crazygames.com/t/fireboy-and-watergirl">Fireboy and Watergirl</a>, <a target="_blank" href="https://www.crazygames.com/t/2-player">2-player games</a>, <a target="_blank" href="https://www.crazygames.com/t/horror">horror</a>, and <a target="_blank" href="https://www.crazygames.com/t/mahjong">mahjong.</a></p>
+                    <h2>Suggest a Game</h2>
+                    <p>Is there a game that you love, but you can't find on CrazyGames? <a href="https://www.crazygames.com/contact?topic=suggestGame">Suggest a game</a> to us and we'll see what we can do to get it. You can also <a target="_blank" href="https://www.crazygames.com/contact">contact us</a> for any general suggestions or improvements.</p>
+                    <h2>About CrazyGames</h2>
+                    <p>CrazyGames is a free browser gaming platform founded in 2014 by Raf Mertens. Since then, the platform has grown to over 20 million monthly users. </p>
+                    <p>Our mission is simple - to create a browser-gaming platform that works seamlessly for users around the world, and rewards developers both big and small. Visit our corporate site and learn more <a target="_blank" href="https://about.crazygames.com/">about CrazyGames</a>.</p>
+                    <p>Check our <a target="_blank" href="https://crazygames.recruitee.com/">open job positions</a>, or take a look at our <a target="_blank" href="https://developer.crazygames.com/">game developer platform</a> if you’re interested in submitting a game.</p>
+                    <p>¿Hablas español? Visita nuestra web de <a target="_blank" href="https://www.1001juegos.com/">juegos</a> en español.</p>
+                    <p>Tu parles français ? Découvrez nos <a target="_blank" href="https://www.crazygames.fr/">jeux </a>sur notre site français.</p>
+                    <p>As of January 2023, we are rated 4.7 with 876 reviews on <a target="_blank" rel="external nofollow" href="https://www.trustpilot.com/review/crazygames.com">Trustpilot</a>.</p>
+                </div>
+            </div>
+            <div class="css-1lo4a7f"><span class="css-ieoiqz">Show More</span></div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout.homepage');
 });
 
 Route::get('/test', function () {
