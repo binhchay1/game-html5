52f5fbc0aba7fedb4bbb20081cb02b8808091911
binhchay1 <binhchay1@gmail.com>
2023-07-28 10:08:05 +0700

    change language

--- app/Enums/Locale.php
+++ app/Enums/Locale.php
@@ -6,6 +6,9 @@ final class Locale
 {
     const LIST_FLAG = [
         'vi' => 'svg/flag/vn.svg',
-        'en' => 'svg/flag/gb.svg'
+        'en' => 'svg/flag/gb.svg',
+        'hr' => 'svg/flag/hr.svg',
+        'ro' => 'svg/flag/ro.svg',
+        'th' => 'svg/flag/th.svg',
     ];
 }
