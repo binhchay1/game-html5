d74b391794adbbcc09e365185e1b76f100e602e9
tranthuy240814 <tranthuy240814@gmail.com>
2023-09-08 14:54:26 +0700

    hash url and add nick name

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Repositories\UserRepository;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
